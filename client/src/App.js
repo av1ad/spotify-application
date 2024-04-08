080b4cb65cb9312bde7831a34fdf87aba2fe8bef
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-04-08 15:46:48 -0400

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
+
+
+    
+})
   return (
     <div className="App">
       <a href="http://localhost:8888/login">Log in to spotify</a>
--- index.js
+++ index.js
@@ -59,21 +59,17 @@ app.get("/callback", (req, res) => {
     },
   }).then((response) => {
     if (response.status === 200) {
-      const { access_token, token_type } = response.data;
-      axios
-        .get("https://api.spotify.com/v1/me", {
-          headers: {
-            Authorization: `${token_type} ${access_token}`,
-          },
-        })
-        .then((response) => {
-          res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-        })
-        .catch((error) => {
-          res.send(error);
-        });
+      const { access_token, refresh_token } = response.data;
+
+      const queryParams = querystring.stringify({
+        access_token,
+        refresh_token,
+      });
+
+      // redirect to react app
+      res.redirect(`http://localhost:3000/?${queryParams}`);
     } else {
-      res.send(response);
+      res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`);
     }
   });
 });
@@ -99,8 +95,7 @@ app.get("/refresh_token", (req, res) => {
     })
     .catch((error) => {
       res.send(error);
-    })
-})
-
+    });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
