f1235c52de4968902453b6990e4cffd25b76bd88
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-04-08 15:17:14 -0400

    Added frontend

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="http://localhost:8888/login">Log in to spotify</a>
+    </div>
+  );
+}
+
+export default App;
