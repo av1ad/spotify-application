45ea2e08f240f17a1d7ace1f1d8db0a907e597bf
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-04-09 13:56:13 -0400

    Set up basis for React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { accessToken, logout, getCurrentUserProfile } from "./spotify";
 import { catchErrors } from "./utils";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -17,29 +18,33 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {!token ? (
-          <a className="App-link" href="http://localhost:8888/login">
-            Log in to Spotify
-          </a>
-        ) : (
-          <>
-            <button onClick={logout}>Log Out</button>
-
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>{profile.followers.total} Followers</p>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar" />
+    <>
+      <Router>
+        <Routes>
+          <Route path="/top-artists" element={<h1>Top Artists</h1>} />
+          <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
+          <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
+          <Route path="/playlists" element={<h1>Playlists</h1>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <button onClick={logout}>Log Out</button>
+                {profile && (
+                  <div>
+                    <h1>{profile.display_name}</h1>
+                    <p>{profile.followers.total} Followers</p>
+                    {profile.images.length && profile.images[0].url && (
+                      <img src={profile.images[0].url} alt="Avatar" />
+                    )}
+                  </div>
                 )}
-              </div>
-            )}
-          </>
-        )}
-      </header>
-    </div>
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
