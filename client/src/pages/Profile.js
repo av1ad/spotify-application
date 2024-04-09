6ee674f40c80fe696b7d68c9fc7ca11b50d61a3b
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-04-09 15:45:19 -0400

    Added ability to see playlist

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,7 +6,12 @@ import {
   getTopArtists,
   getTopTracks,
 } from "../spotify";
-import { SectionWrapper, ArtistsGrid, TrackList } from "../components";
+import {
+  SectionWrapper,
+  ArtistsGrid,
+  TrackList,
+  PlaylistsGrid,
+} from "../components";
 import { StyledHeader } from "../styles";
 
 const Profile = () => {
@@ -65,20 +70,18 @@ const Profile = () => {
             </div>
           </StyledHeader>
 
-          {topArtists && topTracks && (
+          {topArtists && topTracks && playlists && (
             <main>
-              <SectionWrapper
-                title="Top artists this month"
-                seeAllLink="/top-artists"
-              >
+              <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
                 <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
               </SectionWrapper>
 
-              <SectionWrapper
-                title="Top tracks this month"
-                seeAllLink="/top-tracks"
-              >
+              <SectionWrapper title="Top tracks this month" seeAllLink="/top-tracks">
                 <TrackList tracks={topTracks.items.slice(0, 5)} />
+              </SectionWrapper>
+
+              <SectionWrapper title="Playlists" seeAllLink="/playlists">
+                <PlaylistsGrid playlists={playlists.items.slice(0, 10)} />
               </SectionWrapper>
             </main>
           )}
