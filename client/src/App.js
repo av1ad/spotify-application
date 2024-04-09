89efd1b5c214db416c37f46e54ca3d859e403bcc
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-04-09 14:04:54 -0400

    Fixed axios error

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import { useEffect, useState } from "react";
 import { accessToken, logout, getCurrentUserProfile } from "./spotify";
 import { catchErrors } from "./utils";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import "./App.css";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [token, setToken] = useState(null);
@@ -18,16 +33,25 @@ function App() {
   }, []);
 
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/top-artists" element={<h1>Top Artists</h1>} />
-          <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
-          <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
-          <Route path="/playlists" element={<h1>Playlists</h1>} />
-          <Route
-            path="/"
-            element={
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <a className="App-link" href="http://localhost:8888/login">
+              Log in to Spotify
+            </a>
+          }
+        />
+        <Route path="/top-artists" element={<h1>Top Artists</h1>} />
+        <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
+        <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
+        <Route path="/playlists" element={<h1>Playlists</h1>} />
+        <Route
+          path="/"
+          element={
+            token ? (
               <>
                 <button onClick={logout}>Log Out</button>
                 {profile && (
@@ -40,11 +64,15 @@ function App() {
                   </div>
                 )}
               </>
-            }
-          />
-        </Routes>
-      </Router>
-    </>
+            ) : (
+              <a className="App-link" href="http://localhost:8888/login">
+                Log in to Spotify
+              </a>
+            )
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
